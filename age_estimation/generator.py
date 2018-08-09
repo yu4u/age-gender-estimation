2e9daf64437cc01c7f38d88aacfdf08e6d652016
yusuke-a-uchida <yusuke.a.uchida@dena.com>
2018-08-10 00:33:10 +0900

    fix

--- age_estimation/generator.py
+++ age_estimation/generator.py
@@ -13,7 +13,11 @@ def get_transform_func():
     p = Augmentor.Pipeline()
     p.flip_left_right(probability=0.5)
     p.rotate(probability=1, max_left_rotation=5, max_right_rotation=5)
-    p.zoom_random(probability=1, percentage_area=0.95)
+    p.zoom_random(probability=0.5, percentage_area=0.95)
+    p.random_distortion(probability=0.5, grid_width=2, grid_height=2, magnitude=8)
+    p.random_color(probability=1, min_factor=0.8, max_factor=1.2)
+    p.random_contrast(probability=1, min_factor=0.8, max_factor=1.2)
+    p.random_brightness(probability=1, min_factor=0.8, max_factor=1.2)
 
     def transform_image(image):
         image = [Image.fromarray(image)]
