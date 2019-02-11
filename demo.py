a20281dcf5bffbaef3085ecb8857ead41827a92d
nedal <nedal.elghamry@gmail.com>
2019-02-11 13:29:34 +0200

    fix opposite gender detection on draw results

--- demo.py
+++ demo.py
@@ -124,7 +124,7 @@ def main():
             # draw results
             for i, d in enumerate(detected):
                 label = "{}, {}".format(int(predicted_ages[i]),
-                                        "F" if predicted_genders[i][0] < 0.5 else "M")
+                                        "M" if predicted_genders[i][0] < 0.5 else "F")
                 draw_label(img, (d.left(), d.top()), label)
 
         cv2.imshow("result", img)
