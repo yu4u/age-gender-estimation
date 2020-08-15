a523e0d6a65dc4dea20ca23904ae67a059c8f819
Yusuke Uchida <yusuke.a.uchida@dena.com>
2020-08-16 01:04:21 +0900

    path fix

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import hydra
+from hydra.utils import to_absolute_path
 from tensorflow.keras.callbacks import LearningRateScheduler, ModelCheckpoint
 
 from src.factory import get_model, get_optimizer, get_scheduler
@@ -10,7 +11,7 @@ from src.generator import ImageSequence
 
 @hydra.main(config_path="src/config.yaml")
 def main(cfg):
-    csv_path = Path(__file__).resolve().parent.joinpath("meta", f"{cfg.data.db}.csv")
+    csv_path = Path(to_absolute_path(__file__)).parent.joinpath("meta", f"{cfg.data.db}.csv")
     df = pd.read_csv(str(csv_path))
     train, val = train_test_split(df, random_state=42, test_size=0.1)
     train_gen = ImageSequence(cfg, train)
