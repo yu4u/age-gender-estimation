68d0ecb1a57778ac88605ae58f02a24d296f3a9a
Yusuke Uchida <yusuke.a.uchida@dena.com>
2020-08-21 21:58:27 +0900

    change trained model

--- demo.py
+++ demo.py
@@ -9,8 +9,8 @@ from tensorflow.keras.utils import get_file
 from src.factory import get_model
 
 
-pretrained_model = "https://github.com/yu4u/age-gender-estimation/releases/download/v0.5/weights.28-3.73.hdf5"
-modhash = 'fbe63257a054c1c5466cfd7bf14646d6'
+pretrained_model = "https://github.com/yu4u/age-gender-estimation/releases/download/v0.6/EfficientNetB3_224_weights.11-3.44.hdf5"
+modhash = '6d7f7b7ced093a8b3ef6399163da6ece'
 
 
 def get_args():
